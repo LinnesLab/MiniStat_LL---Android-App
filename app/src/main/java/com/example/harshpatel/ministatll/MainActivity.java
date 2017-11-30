38c420fd51ab78d97f0f9994148c62a95a159cce
Harshpatel682 <patel682@purdue.edu>
2017-11-30 13:08:32 -0500

    these are the most recent docs, just thought i'd transfer the newest files to the Linnes Lab git-repo

--- app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
+++ app/src/main/java/com/example/harshpatel/ministatll/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.harshpatel.ministatll;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,18 +16,34 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
+    public boolean isConnected = false;
+    private BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
+    String deviceAddress;
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    private BluetoothGattCharacteristic characteristic;
+    //private BluetoothLeService mBluetoothLeService;
 
-    Spinner spinner;
-    ArrayAdapter<CharSequence> adapter;
+
+
+    Spinner spinner; //drop down list
+    ArrayAdapter<CharSequence> adapter;//returns the value from out spinner
     TextView selectedExperiment; //will store the textview of the selected experiment
 
-    public Button selectedExperimentButton;
+    public Button selectedExperimentButton; //this button will send the user to the specified experiment page
 
-    
     public void init() {
         selectedExperimentButton = (Button) findViewById(R.id.selectedExperimentButton);
+        /*when ever the continue button is pressed, this will use the value from the
+        * spinner to go to the specified experiment page
+        */
         selectedExperimentButton.setOnClickListener(new View.OnClickListener() {
             Intent openSelectedExperimentPage;
+            //when the button is clicked it will see what experiment name is stored in "selectedExperiment"
             @Override
             public void onClick(View v) {
                 switch ((String)selectedExperiment.getText()) {
@@ -47,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                         openSelectedExperimentPage = new Intent(MainActivity.this, pv_input_window.class);
                         break;
                 }
-                startActivity(openSelectedExperimentPage);
+                startActivity(openSelectedExperimentPage); //this will send the user to the one of the experiment pages
             }
         });
     }
@@ -56,22 +75,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        init(); //sets up the above code onCreate
 
+        /*Initializing the spinner and adapter */
         spinner = (Spinner)findViewById(R.id.dropDownForExperiments);
         adapter = ArrayAdapter.createFromResource(this,R.array.experiments,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        //this will set the "selectedExperiment" to the experiment selected from the dropdown list
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedExperiment = (TextView) view;
+                selectedExperiment = (TextView) view; //set the value received from the spinner(drop down)
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                //does not have a null/nothing option on the drop down
             }
         });
     }
+
 }
