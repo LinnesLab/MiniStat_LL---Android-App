a0db0bb2fbd8253f03958e27e4b5cf69f781ca0a
Harshpatel682 <patel682@purdue.edu>
2017-12-08 17:29:29 -0500

    added internal storage of the csv file and it sending to someone via email, i still need to test it on the actual android device

--- app/src/main/java/com/example/harshpatel/ministatll/graph_view_example.java
+++ app/src/main/java/com/example/harshpatel/ministatll/graph_view_example.java
@@ -1,12 +1,15 @@
 package com.example.harshpatel.ministatll;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Interpolator;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.Viewport;
@@ -14,10 +17,21 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.PointsGraphSeries;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class graph_view_example extends AppCompatActivity {
+
+    //this gets the date and time, because you wanna remember when you did the experiment
+    Calendar calendar;
+    SimpleDateFormat simpleDateFormat;
+    String date;
 
     private static final String TAG = "graph_view_example";
 
@@ -47,6 +61,9 @@ public class graph_view_example extends AppCompatActivity {
             3762 ,3045, 2567 ,2328 ,2209 ,2090 ,2090 ,1851 ,1373 ,-656 ,
             -5434 ,-9495, -8539 ,-7464};
 
+    double[] target_x;
+    double[] target_y;
+
     int currentPosition;
     PointsGraphSeries<DataPoint> dataSeries;
     GraphView scatterPlot;
@@ -59,6 +76,7 @@ public class graph_view_example extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_view_example);
+
 
         scatterPlot = (GraphView) findViewById(R.id.scatterPlot);
 //        dataSeries = new PointsGraphSeries<>();
@@ -76,6 +94,10 @@ public class graph_view_example extends AppCompatActivity {
         }
 
         dataSeries = new PointsGraphSeries<>();
+
+        if (end_of_list) {
+            add_points_button.setEnabled(false);
+        }
 
         if (!end_of_list) {
             add_points_button.setOnClickListener(new View.OnClickListener() {
@@ -103,11 +125,19 @@ public class graph_view_example extends AppCompatActivity {
             end_of_list = true;
         }
 
+        if (end_of_list) {
+            add_points_button.setEnabled(false);
+            Toast.makeText(getBaseContext(), "There are no more values to add...", Toast.LENGTH_LONG).show();
+
+        }
+
         if (!end_of_list) {
             valueArray.add(new XYValue(xs[currentPosition], ys[currentPosition]));
             Log.e(TAG, "adding " + xs[currentPosition] + " " + ys[currentPosition] + " position " + currentPosition);
             valueArray = sortArray(valueArray);
 
+            target_x[currentPosition] = xs[currentPosition];
+            target_y[currentPosition] = ys[currentPosition];
             currentPosition++;
         }
 
@@ -194,5 +224,41 @@ public class graph_view_example extends AppCompatActivity {
             }
         }
         return array;
+    }
+
+    public void save_csv_and_send(View view) {
+        calendar = Calendar.getInstance();
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date = simpleDateFormat.format(calendar.getTime());
+
+        String data ="" + date + "\n";
+        for (int i = 0; i < xs.length; i++) {
+            data += target_x[i] + ',' + target_y[i] + "\n";
+        }
+
+        String file_name = "experiment.csv";
+        try {
+            FileOutputStream fileOutputStream = openFileOutput(file_name, MODE_PRIVATE);
+            fileOutputStream.write(data.getBytes());
+            fileOutputStream.close();
+            Toast.makeText(getBaseContext(), "Values are saved into experiment.csv", Toast.LENGTH_LONG).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setType("*/*");
+        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[] {"sample_email@gmail.com"});
+        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                "Experiment Performed");
+        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
+                "go on read the emails");
+
+        Uri uri = Uri.fromFile(new File(file_name));
+        emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
     }
 }
